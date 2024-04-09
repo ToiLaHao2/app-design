cccccb248b8e87e1d6893421c51b84e7560e3bb3
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-04-09 23:55:57 +0700

    up

--- LEX DESIGN/TraGo Manager/data.js
+++ LEX DESIGN/TraGo Manager/data.js
@@ -5,3 +5,20 @@ let managers = [
         password: "123",
     },
 ];
+let reports = [
+    {
+        reportId: "r1",
+        status: "unknown",
+        issue: "bad bin",
+        image: "",
+        description: "this is a bad bin",
+    },
+];
+function login(account, password) {
+    for (let manager of managers) {
+        if (manager.account == account && manager.password == password) {
+            return 1;
+        }
+    }
+    return 0;
+}
